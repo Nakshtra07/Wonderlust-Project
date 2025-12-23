e04023ab48debdbeab77a5e2852cecefc794f97e
Nakshatra <nakshtraprajapati838@gmail.com>
2025-12-23 14:49:56 +0530

    fixing error part-89

--- app.js
+++ app.js
@@ -98,12 +98,8 @@ app.use((req, res, next) => {
 // ROOT ROUTE (IMPORTANT)
 // =======================
 app.get("/", (req, res) => {
-    res.status(200).json({
-        status: "ok",
-        message: "Wanderlust API is running",
-    });
+    res.redirect("/listings");
 });
-
 
 
 // =======================
